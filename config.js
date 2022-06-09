9ed45a852e2bbb71d3e61a07c83c79e19d6d3edf
Daniel Wood <theamazingfedex@users.noreply.github.com>
2022-06-09 10:45:25 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  envFilename: '.env',
+  paths: {
+    gateway: '/Users/daniel.wood/Workspace/gateway-graphql-server/',
+    falcon: '/Users/daniel.wood/Workspace/graphql-server/',
+    // any other projects with an appropriately formatted .env file can go here
+    // keys are used by the CLI to allow only changing specified projects if desired
+  },
+};
+module.exports = config;
